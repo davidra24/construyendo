b830db40a3a2bc786808e6adcfe189f7d9a7cf97
David Ramírez <cdavidramirezg24@gmail.com>
2019-07-22 14:17:31 -0500

    path

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const path = require('path');
 require('./drivers/mongo-connection');
+const bundle = '../../dist/index.html';
 const api = require('./api/api');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static('dist'));
 app.listen(port, () => console.log(`Listening on port ${port}!`));
 
 app.use('/api', api);
+
+app.get('*', (req, res) => {
+  const index = path.join(__dirname, bundle);
+  res.sendFile(index);
+});
