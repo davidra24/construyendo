72a7d068281079e7203c5820de5c47eef09bbee9
David Ramírez <cdavidramirezg24@gmail.com>
2019-07-22 14:07:31 -0500

    package heroku

--- src/server/index.js
+++ src/server/index.js
@@ -5,7 +5,7 @@ const api = require('./api/api');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.static('dist'));
+app.use(express.static(__dirname + '/dist'));
 app.listen(port, () => console.log(`Listening on port ${port}!`));
 
 app.use('/api', api);
